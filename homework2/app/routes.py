c84a5798b19e65400e45696c70342df2212a5716
Jack Dauphars <jdauphar@kent.edu>
2018-04-04 21:41:19 -0400

    completed thru tutorial 4

--- homework2/app/routes.py
+++ homework2/app/routes.py
@@ -1,11 +1,14 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
+
+@app.route('/')
 @app.route('/user/<name>')
 def index(name=None):
-    user = {'username': 'Steve'}
-    if name:
-        user = {'username': name}
+    user = {'username': name}
+    if name == None:
+        user = {'username': 'Jack'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -17,3 +20,12 @@ def index(name=None):
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
